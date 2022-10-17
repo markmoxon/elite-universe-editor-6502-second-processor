5b29a1cc6ab745ae11e64da49b825e58587ba183
Mark Moxon <mark@moxon.net>
2022-10-17 23:36:54 +0100

    Add more box art

--- 1-source-files/main-sources/elite-disc.asm
+++ 1-source-files/main-sources/elite-disc.asm
@@ -71,4 +71,5 @@ PUTFILE "3-assembled-output/README.txt", "README", &FFFFFF, &FFFFFF
 
 \ Universe files added for Universe Editor
 
+PUTFILE "1-source-files/universe-files/U.BOXART1.bin", "U.BOXART1", &000000, &000000
 PUTFILE "1-source-files/universe-files/U.BOXART2.bin", "U.BOXART2", &000000, &000000
