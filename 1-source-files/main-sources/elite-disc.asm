4ea1a4bd7325d9ea45d6ff3aa339d6fa11282f4e
Mark Moxon <mark@moxon.net>
2022-10-24 12:19:27 +0100

    Remove test box art file

--- 1-source-files/main-sources/elite-disc.asm
+++ 1-source-files/main-sources/elite-disc.asm
@@ -74,4 +74,3 @@ PUTFILE "3-assembled-output/README.txt", "README", &FFFFFF, &FFFFFF
 PUTFILE "1-source-files/universe-files/U.BOXART1.bin", "U.BOXART1", &000000, &000000
 PUTFILE "1-source-files/universe-files/U.BOXART2.bin", "U.BOXART2", &000000, &000000
 PUTFILE "1-source-files/universe-files/U.PAGE5.bin", "U.PAGE5", &000000, &000000
-PUTFILE "1-source-files/universe-files/U.BOXARTE.bin", "U.BOXARTE", &000000, &000000
