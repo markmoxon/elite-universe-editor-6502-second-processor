128a180c0a1fa4bb4cf8d9895b669147eff72e1f
Mark Moxon <mark@moxon.net>
2022-12-07 13:59:04 +0000

    Add C64 version flag

--- 1-source-files/main-sources/elite-z.asm
+++ 1-source-files/main-sources/elite-z.asm
@@ -8702,6 +8702,7 @@ CPU 0                   \ Switch back to normal 6502 asembly
 
 _6502SP_VERSION = TRUE
 _MASTER_VERSION = FALSE
+_C64_VERSION    = FALSE
 
 INCLUDE "1-source-files/main-sources/elite-universe-editor-z.asm"
 
